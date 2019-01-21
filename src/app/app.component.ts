9fb2bfb69353e07f75ec65548af5dd5b82d8d4db
Amitai Barnea <amitai@spectory.com>
2019-01-21 15:37:33 +0200

    add api cache

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,47 @@
-import { Component } from '@angular/core';
-import { ngswAppInitializer } from '@angular/service-worker/src/module';
-import * as moment from 'moment';
+import { Component } from "@angular/core";
+import { ngswAppInitializer } from "@angular/service-worker/src/module";
+import * as moment from "moment";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
   forecast = [];
-  city: string = 'London';
-  cityTitle: string = 'London';
+  city: string = "London";
+  cityTitle: string = "London";
+  error: string = "";
 
   ngOnInit() {
     this.fetchForecast();
   }
 
   async fetchForecast() {
-    const res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${this.city}&units=metric&APPID=b1f79e77b2b077ea0ff26a650d8432af`);
-    const json = await res.json();
+    this.error = "";
+    try {
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${
+          this.city
+        }&units=metric&APPID=b1f79e77b2b077ea0ff26a650d8432af`
+      );
+      const json = await res.json();
 
-    this.forecast =  json.list.filter((l) => moment(l.dt_txt).hour() === 12).map((dayForecast) => {
-      return {
-        icon: `http://openweathermap.org/img/w/${dayForecast.weather[0].icon}.png`,
-        description: dayForecast.weather[0].description,
-        temp: Math.round(dayForecast.main.temp),
-        date: moment(dayForecast.dt_txt).format("ddd")
-      };
-    });
-    this.cityTitle = this.city;
+      this.forecast = json.list
+        .filter(l => moment(l.dt_txt).hour() === 12)
+        .map(dayForecast => {
+          return {
+            icon: `http://openweathermap.org/img/w/${
+              dayForecast.weather[0].icon
+            }.png`,
+            description: dayForecast.weather[0].description,
+            temp: Math.round(dayForecast.main.temp),
+            date: moment(dayForecast.dt_txt).format("ddd")
+          };
+        });
+      this.cityTitle = this.city;
+    } catch (error) {
+      this.error = "Could not fetch weather";
+    }
   }
 }
