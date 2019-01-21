96deec9f74ff5a9ea03bcf7fee5e793995e470a9
Amitai Barnea <amitai@spectory.com>
2019-01-21 14:41:36 +0200

    add weather

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ngswAppInitializer } from '@angular/service-worker/src/module';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-weather-app';
+  forecast = [];
+  city: string = 'London';
+  cityTitle: string = 'London';
+
+  ngOnInit() {
+    this.fetchForecast();
+  }
+
+  async fetchForecast() {
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${this.city}&units=metric&APPID=b1f79e77b2b077ea0ff26a650d8432af`);
+    const json = await res.json();
+
+    this.forecast =  json.list.filter((l) => moment(l.dt_txt).hour() === 12).map((dayForecast) => {
+      return {
+        icon: `http://openweathermap.org/img/w/${dayForecast.weather[0].icon}.png`,
+        description: dayForecast.weather[0].description,
+        temp: Math.round(dayForecast.main.temp),
+        date: moment(dayForecast.dt_txt).format("ddd")
+      };
+    });
+    this.cityTitle = this.city;
+  }
 }
